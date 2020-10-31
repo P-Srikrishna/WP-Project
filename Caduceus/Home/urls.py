d7a07aecb6b9dd234a055915df466517510e0a9b
P-Srikrishna <srikrishna.p@svkmmumbai.onmicrosoft.com>
2020-10-31 22:47:28 +0530

    commit to 'main'

--- Caduceus/Home/urls.py
+++ Caduceus/Home/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from Home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("Home.urls"))
+    path('', views.homepage, name="home"),
 ]
