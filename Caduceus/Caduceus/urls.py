09b0e22f2bbbf130ebe070cc3ac846b09ec6d79d
P-Srikrishna <srikrishna.p@svkmmumbai.onmicrosoft.com>
2020-11-01 00:17:20 +0530

    commit to 'main'

--- Caduceus/Caduceus/urls.py
+++ Caduceus/Caduceus/urls.py
@@ -13,7 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+admin.site.site_header = "Caduceus Admin"
+admin.site.site_title = "Caduceus Admin Portal"
+admin.site.index_title = "Welcome to Caduceus Researcher Portal"
 from django.urls import path, include
 
 urlpatterns = [
