d3f6a40bc02a70a0ef26b9a4ce845b353cc9a916
P-Srikrishna <srikrishna.p@svkmmumbai.onmicrosoft.com>
2020-11-02 02:48:02 +0530

    commit to 'main'

--- Caduceus/Home/urls.py
+++ Caduceus/Home/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Home import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
