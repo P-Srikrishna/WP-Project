67e7f12e82b716dec41c558029f97c7e3485f000
Sarvesh Agarwal <53811121+sarveshag25@users.noreply.github.com>
2020-11-02 06:23:14 +0530

    databsae1

--- Caduceus/Home/models.py
+++ Caduceus/Home/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class registration(models.Model): 
+class Registration1(models.Model): 
     FirstName   = models.CharField(max_length =120)
     LastName    = models.CharField(max_length =120)
     Email       = models.CharField(max_length =120)
     Password    = models.CharField(max_length =16)
     Address     = models.CharField(max_length =120)
-    PhoneNumber = models.CharField(max_length =12 )
-    City        = models.CharField(max_length =120)
+    PhoneNumber = models.CharField(max_length =12)
+    City        = models.CharField(('City'), max_length =120)
     date        = models.DateField()
     
